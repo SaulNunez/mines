0d1cebac8365baf253da41602823c2477ad088b9
Saúl Núñez <saul.nunez99@gmail.com>
2025-03-14 11:42:37 -0600

    WIP Style game

--- src/app/mines/index.tsx
+++ src/app/mines/index.tsx
@@ -9,35 +9,18 @@ export default function Mines(){
     const gridSize = useSelector<GameState, number>((state) => state.gridSize);
     const grid = useSelector<GameState, Square[]>((state) => state.board.flat());
     const dispatch = useDispatch<Dispatch<GameAction>>();
-    
-    const renderItem = ({item}) => {
-        const itemSize = 32;
-        if(item.state == SquareState.FLAGGED) {
-            return (
-                <Pressable onPress={() => dispatch({type: 'UNFLAG', index_x: item.x, index_y: item.y})}>
-                    <View style={[styles.item, { width: itemSize, height: itemSize }]}>
-                        <Text>🚩</Text>
-                    </View>
-                </Pressable>
-    
-            );
-        } else if (item.state == SquareState.OPENED){
-            return (
-                    <View style={[styles.item, { width: itemSize, height: itemSize }]}>
-                        {item.hasMine? <Text>💣</Text> : <Text>{item.nearMines}</Text>}
-                    </View>
-            );
-        } else {
-            return (
-                <Pressable 
-                    onPress={() => dispatch({type: 'CLICK', index_x: item.x, index_y: item.y})}
-                    onLongPress={() => dispatch({type: 'FLAG', index_x: item.x, index_y: item.y})}>
-                    <View style={[styles.item, { width: itemSize, height: itemSize }]}>
-                        <Text></Text>
-                    </View>
-                </Pressable>
-            );
+
+    const itemContent = (item: Square) => {
+        if(item.state == SquareState.FLAGGED){
+            return "🚩";
         }
+        if(item.state == SquareState.OPENED && item.hasMine){
+            return "💣";
+        }
+        if(item.state == SquareState.OPENED && item.nearMines > 0){
+            return item.nearMines;
+        }
+        return "";
     }
 
     useEffect(() => {
@@ -48,7 +31,22 @@ export default function Mines(){
         <FlatList
             data={grid} 
             numColumns={9}
-            renderItem={renderItem}
+            renderItem={({item}) => {
+                const itemSize = 64;
+        
+                return (
+                    <Pressable 
+                        disabled={item.state === SquareState.OPENED}
+                        onLongPress={() => dispatch({type: 'UNFLAG', index_x: item.x, index_y: item.y})}
+                        onPress={() => dispatch({type: 'CLICK', index_x: item.x, index_y: item.y})}>
+                        <View style={[item.state === SquareState.OPENED? styles.itemDisabled : styles.item, 
+                            { width: itemSize, height: itemSize }]}>
+                            <Text style={styles.text}>{itemContent(item)}</Text>
+                        </View>
+                    </Pressable>
+        
+                );
+            }}
             scrollEnabled={false}
         />
     );
@@ -56,13 +54,24 @@ export default function Mines(){
 
 const styles = StyleSheet.create({
     item: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#4CAF50',
-      margin: 2,
+        borderRadius: 16,
+        borderColor: '#e5e5e5',
+        borderStyle: 'solid',
+        borderWidth: 2,
+        borderBottomWidth: 6,
+        borderBottomColor: '#e5e5e5',
+        margin: 10,
+        alignItems: 'stretch'
+    },
+    itemDisabled: {
+        margin: 10,
+        borderRadius: 16,
+        backgroundColor: '#dddddd',
+        alignItems: 'stretch'
     },
     text: {
-      color: 'white',
-      fontSize: 18,
+        textAlign: 'center',
+        fontSize: 19,
+        color: '#4b4b4b'
     },
   });
